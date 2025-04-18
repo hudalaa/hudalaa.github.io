9d356a3264afeb703480d16d77dd5f1229d3f8b3
unknown <houda.laaziz.mr@gmail.com>
2025-04-18 17:21:56 +0100

    instaling tailwindcss@3 correctly/ fixing input/output files problem

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./public/**/*.html", // Adjust this path to match your HTML files
+    "./src/**/*.{js,ts,jsx,tsx}", // Adjust this path to match your JS/TS files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
