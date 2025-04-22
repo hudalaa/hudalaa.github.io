e1fe5909f323e2dae02afc279220b7ed5c6d84c2
unknown <houda.laaziz.mr@gmail.com>
2025-04-22 22:07:28 +0100

    changing public folder name to docs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./doc/**/*.html", // Adjust this path to match your HTML files
+    "./docs/**/*.html", // Adjust this path to match your HTML files
     "./src/**/*.{js,ts,jsx,tsx}", // Adjust this path to match your JS/TS files
   ],
   theme: {
