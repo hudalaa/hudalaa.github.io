38f0d94eb1a7f536426222872b157ecceac122d4
unknown <houda.laaziz.mr@gmail.com>
2025-04-23 21:17:48 +0100

    customized colors in config.tailwind.js / changed hex colors in index into configured colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,37 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}", // Adjust this path to match your JS/TS files
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Custom colors can be defined here
+        primary: "#1E3A8A", // Example color
+
+        Plviolet: "#ebcfff",
+        Pbviolet: "#a278ce",
+
+        Plbleu: "#ccebff",
+        Pbbleu: "#21a5dd",
+
+        Plgreen: "#eaffdc",
+        Pbgreen: "#4baa7a",
+
+        Plorange: "#ffdca8",
+        Pborange: "#f5c379",
+
+        Plyellow: "#fff4c0",
+        Pbyellow: "#e8cb12",
+
+        Plpeach: "#ffd4c4",
+        Pbpeach: "#f48c54",
+
+        Plred: "#ff9d98",
+        Pbred: "#da434f",
+
+        Plpink: "#ffe3ef",
+        Pbpink: "#e95a87"
+      },
+    
+    },
   },
   plugins: [],
 }
